51c361939b3e2ebe77b791a42c354c74eb4ce0d3
ASAD <asadullah5368@gmail.com>
2024-11-30 20:24:53 +0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import connectDB from "./database/db.js";
+import userRoutes from "./routes/user.routes.js";
+import cookieParser from "cookie-parser";
+
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+app.use("/api/user", userRoutes);
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+  connectDB();
+});
