7d3df6f0aecf1d355ebccdf4f7069ed0e69b452e
ASAD <asadullah5368@gmail.com>
2024-12-01 06:48:01 +0500

    Shadcn implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Button } from "./components/ui/button";
 
 const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline"> Home Page</h1>
+      <Button>Click Me</Button>
     </>
   );
 };
--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         const connection = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${connection.connection.name}`)
+        console.log(`MongoDB Connected: ${connection.connection.host}`);
     } catch (error) {
         console.log(error);
     }
